09529eaf86a6fd23530ff49d858a3aaa0e907e1c
Artem Votintsev <artem.votincev@gmail.com>
2022-11-28 21:47:22 +0100

    Fix achievements appearance

--- src/gd-achievements/AchievementNotification.cs
+++ src/gd-achievements/AchievementNotification.cs
@@ -4,10 +4,22 @@ using Godot;
 [SceneReference("AchievementNotification.tscn")]
 public partial class AchievementNotification
 {
+    [Export]
+    public float ShowTime {get;set;} = 2;
+    
+    [Signal]
+    public delegate void AnimationFinished();
+
     public override void _Ready()
     {
         base._Ready();
         this.FillMembers();
+
+        this.animationPlayer.Connect("animation_finished", this, nameof(AnimationFinishedInternal));
+    }
+
+    private void AnimationFinishedInternal(string name) {
+        this.EmitSignal(nameof(AnimationFinished));
     }
 
     public void SetAchievement(Achievement data)
@@ -16,12 +28,12 @@ public partial class AchievementNotification
         this.textureRect.Texture = ResourceLoader.Load<Texture>(data.IconPath);
     }
 
-    public void on_show()
+    public void ShowAchievement()
     {
         this.animationPlayer.Play("popup");
     }
 
-    public void on_hide()
+    public void HideAchievement()
     {
         this.animationPlayer.Play("hide");
     }
--- src/gd-achievements/AchievementNotifications.cs
+++ src/gd-achievements/AchievementNotifications.cs
@@ -2,60 +2,34 @@ using Godot;
 using GodotAnalysers;
 using Antilines.Presentation.Utils;
 
-[SceneReference("AchievementNotification.tscn")]
+[SceneReference("AchievementNotifications.tscn")]
 public partial class AchievementNotifications
 {
-    public enum GROW_DIRECTIONS
-    {
-        UP,
-        DOWN,
-        LEFT,
-        RIGHT,
-    }
-    public enum POSITIONS
-    {
-        TOP_LEFT,
-        TOP_RIGHT,
-        BOTTOM_LEFT,
-        BOTTOM_RIGHT,
-    }
-
+    [Export]
+    public float ShowTime{get; set;} = 1;
 
     [Export]
-    public float ShowTime = 4.7f;
+    public AudioStream GlobalSound
+    {
+        get { return this.audioStreamPlayer.Stream; }
+        set { this.audioStreamPlayer.Stream = value; }
+    }
+
     [Export]
-    public float HideTime = 1.5f;
+    public float GlobalSoundVolume
+    {
+        get { return this.audioStreamPlayer.VolumeDb; }
+        set { this.audioStreamPlayer.VolumeDb = value; }
+    }
     [Export]
-    AudioStream GlobalSound = ResourceLoader.Load<AudioStream>("res://gd-achievements/resources/sounds/achievement_earned.wav");
-    [Export]
-    public float GlobalSoundVolume = -20.0f;
-    [Export]
-    public GROW_DIRECTIONS grow_direction = GROW_DIRECTIONS.DOWN;
-    [Export]
-    public POSITIONS position = POSITIONS.TOP_LEFT;
+    public PackedScene AchievementNotificationScene;
 
     private IAchievementRepository achievementRepository = new LocalAchievementRepository();
-    private int achievementCount = 0;
-    private AudioStreamPlayer soundNode = new AudioStreamPlayer();
-    private PackedScene AchievementNotificationScene = ResourceLoader.Load<PackedScene>("res://gd-achievements/AchievementNotification.tscn");
-
-    private void InitSoundNode()
-    {
-        soundNode.Stream = GlobalSound;
-        soundNode.VolumeDb = GlobalSoundVolume;
-        AddChild(soundNode);
-    }
 
     public override void _Ready()
     {
-        InitSoundNode();
-
-        SetAnchorsPreset(LayoutPreset.Wide, false);
-
-        MarginTop = 0;
-        MarginBottom = 0;
-        MarginLeft = 0;
-        MarginRight = 0;
+        base._Ready();
+        FillMembers();
     }
 
     public void ProgressAchievement(string key, int progress)
@@ -83,56 +57,18 @@ public partial class AchievementNotifications
     {
         GD.Print($"Achievement System: Show achievement '{data.Name}'");
 
-        var notification_instance = (AchievementNotification)AchievementNotificationScene.Instance();
-        AddChild(notification_instance);
+        var notification = (AchievementNotification)AchievementNotificationScene.Instance();
+        this.achievementsContainer.AddChild(notification);
+        notification.SetAchievement(data);
 
-        achievementCount += 1;
+        this.audioStreamPlayer.Play();
 
-        if (achievementCount > 1)
-        {
-            switch (grow_direction)
-            {
-                case GROW_DIRECTIONS.UP:
-                    notification_instance.RectPosition = new Vector2(notification_instance.RectPosition.x, (1 - achievementCount) * (notification_instance.RectSize.y));
-                    break;
-                case GROW_DIRECTIONS.DOWN:
-                    notification_instance.RectPosition = new Vector2(notification_instance.RectPosition.x, (achievementCount - 1) * (notification_instance.RectSize.y));
-                    break;
-                case GROW_DIRECTIONS.LEFT:
-                    notification_instance.RectPosition = new Vector2((1 - achievementCount) * (notification_instance.RectSize.x), notification_instance.RectPosition.y);
-                    break;
-                case GROW_DIRECTIONS.RIGHT:
-                    notification_instance.RectPosition = new Vector2((achievementCount - 1) * (notification_instance.RectSize.x), notification_instance.RectPosition.y);
-                    break;
-            }
-        }
-
-        switch (position)
-        {
-            case POSITIONS.TOP_RIGHT:
-                notification_instance.RectPosition = new Vector2(notification_instance.RectPosition.x + RectSize.x - notification_instance.RectSize.x, notification_instance.RectPosition.y);
-                break;
-            case POSITIONS.BOTTOM_RIGHT:
-                notification_instance.RectPosition = new Vector2(notification_instance.RectPosition.x + RectSize.x - notification_instance.RectSize.x, notification_instance.RectPosition.y + RectSize.y - notification_instance.RectSize.y);
-                break;
-            case POSITIONS.BOTTOM_LEFT:
-                notification_instance.RectPosition = new Vector2(notification_instance.RectPosition.x, notification_instance.RectPosition.y + RectSize.y - notification_instance.RectSize.y);
-                break;
-        }
-        notification_instance.SetAchievement(data);
-
-        soundNode.Play();
-
-        notification_instance.on_show();
-
+        notification.ShowAchievement();
+        await ToSignal(notification, nameof(AchievementNotification.AnimationFinished));
         await ToSignal(GetTree().CreateTimer(ShowTime), "timeout");
+        notification.HideAchievement();
+        await ToSignal(notification, nameof(AchievementNotification.AnimationFinished));
 
-        achievementCount -= 1;
-
-        notification_instance.on_hide();
-
-        await ToSignal(GetTree().CreateTimer(HideTime), "timeout");
-
-        notification_instance.QueueFree();
+        notification.QueueFree();
     }
 }
